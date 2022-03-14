6b6f473bafbe483dae5e138b06ea26a95583bc39
Mateusz K <68960340+Snive6@users.noreply.github.com>
2022-03-14 16:19:38 +0100

    Add files via upload

---
+++ points_generator.py
@@ -0,0 +1,21 @@
+from random import randrange, randint, choice
+
+
+def points_generator():
+    n = randint(400, 600)
+    points = []
+    types_of_goods = ['tuna', 'orange', 'uranium']
+    not_repeated = []
+    for i in range(n):
+        x = randrange(100)
+        y = randrange(100)
+        if [x, y] in not_repeated:
+            continue
+        not_repeated.append([x, y])
+        waga_towaru = randint(100, 200)
+        rodzaj_towaru = choice(types_of_goods)
+        points.append([x, y, waga_towaru, rodzaj_towaru])
+    return points
+
+
+print(points_generator())
