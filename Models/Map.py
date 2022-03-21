7f01fca04f821599f2cdc86407800bea6af1c962
Marcin Filapek <filap4@gmail.com>
2022-03-21 22:19:02 +0100

    try to generate all distances

--- Models/Map.py
+++ Models/Map.py
@@ -1,12 +1,26 @@
-from ast import List
+import math
+from typing import Generator
 from Models.Point import Point
 
 
 class Map:
-    def __init__(self, points: List(Point)):
-        self.__points = points
+    def __init__(self, points: Generator[Point, None, None]):
+        self.__points = list(points)
         pass
 
     @property
-    def points(self) -> List(Point):
+    def points(self):
         return self.__points
+
+    def distances(self):
+        # ) -> Dict[Tuple[int, int, int, int], float]:
+        for (idx, point) in enumerate(self.__points):
+            if idx == len(self.__points):
+                continue
+            for point2 in self.__points[idx + 1 :]:
+                yield {
+                    (point.x, point.y, point2.x, point2.y),
+                    math.sqrt(
+                        (point.x - point2.x) ** 2 + (point.y - point2.y) ** 2
+                    ),
+                }
--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from Generators.points_generator import points_generator
 
 map = Map(points_generator())
 
-[print(next(map.points)) for _ in range(20)]
+# [print(next(map.points)) for _ in range(20)]
+
+[print(next(map.distances())) for _ in range(2)]
